f6f009c8feb81bf96a5d99e1123ca6757857c11f
YoshuaAditya <yoshuaadityakurnia@gmail.com>
2023-01-27 15:40:50 +0700

    Add socket connection
    Barebones socket connection.
    Still work in progress, later on tidy the code and make it stream chats.

--- app/src/main/java/com/example/composer/MainActivity.kt
+++ app/src/main/java/com/example/composer/MainActivity.kt
@@ -1,9 +1,18 @@
 package com.example.composer
 
+import android.app.Service
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import android.content.pm.PackageManager
 import android.content.res.Configuration
+import android.net.nsd.NsdManager
+import android.net.nsd.NsdServiceInfo
 import android.os.Build
 import android.os.Bundle
+import android.os.IBinder
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
@@ -21,10 +30,7 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Create
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
@@ -43,6 +49,7 @@ import androidx.navigation.compose.rememberNavController
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.example.composer.data.Chat
+import com.example.composer.retrofit.TcpServerService
 import com.example.composer.ui.theme.ComposerTheme
 import com.example.composer.ui.theme.LightBlue
 import com.example.composer.ui.theme.Red
@@ -50,6 +57,9 @@ import com.example.composer.ui.theme.Teal200
 import com.example.composer.views.*
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+import java.io.DataOutputStream
+import java.net.InetAddress
+import java.net.Socket
 import kotlin.random.Random
 
 
@@ -58,11 +68,204 @@ class MainActivity : ComponentActivity() {
     //weird error? java.lang.IllegalArgumentException: CreationExtras must have a value by `SAVED_STATE_REGISTRY_OWNER_KEY`
     //https://stackoverflow.com/questions/73302605/creationextras-must-have-a-value-by-saved-state-registry-owner-key
     val mainViewModel: MainViewModel by viewModels()
+    lateinit var nsdManager: NsdManager
+    val TAG = "tag"
+    var mServiceName = "NsdChat"
+    val SERVICE_TYPE = "_nsdchat._tcp."
+    var boundService: TcpServerService? = null
+    private var serviceIntent: Intent? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        nsdManager = getSystemService(Context.NSD_SERVICE) as NsdManager
         setContent {
             NavigationHost(this)
+        }
+    }
+
+    fun doBindingService() {
+        val intent = Intent(this, TcpServerService::class.java)
+        serviceIntent = intent
+        bindService(
+            intent, object : ServiceConnection {
+                override fun onServiceConnected(name: ComponentName, service: IBinder?) {
+                    val binder = service as TcpServerService.LocalBinder
+                    boundService = binder.service
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        startForegroundService(intent)
+                    } else {
+                        startService(intent)
+                    }
+                    boundService?.let {
+                        it.startMeForeground()
+                        println("Host listening on port:${it.port}")
+                        registerService(it.port)
+                    }
+                    unbindService(this)
+                }
+
+                override fun onServiceDisconnected(name: ComponentName?) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                        boundService?.stopForeground(Service.STOP_FOREGROUND_REMOVE)
+                    } else {
+                        boundService?.stopForeground(true)
+                    }
+                    boundService = null
+                }
+            },
+            Context.BIND_AUTO_CREATE
+        )
+    }
+
+    fun registerService(port: Int) {
+        // Create the NsdServiceInfo object, and populate it.
+        val serviceInfo = NsdServiceInfo().apply {
+            // The name is subject to change based on conflicts
+            // with other services advertised on the same network.
+            serviceName = "NsdChat"
+            serviceType = "_nsdchat._tcp"
+            setPort(port)
+        }
+        try {
+            nsdManager.apply {
+                registerService(serviceInfo, NsdManager.PROTOCOL_DNS_SD, registrationListener)
+            }
+        } catch (e: java.lang.IllegalArgumentException) {
+            println(e)
+        }
+
+    }
+
+    private val registrationListener = object : NsdManager.RegistrationListener {
+
+        override fun onServiceRegistered(NsdServiceInfo: NsdServiceInfo) {
+            // Save the service name. Android may have changed it in order to
+            // resolve a conflict, so update the name you initially requested
+            // with the name Android actually used.
+            mServiceName = NsdServiceInfo.serviceName
+            Log.d(TAG, "Service Registered $mServiceName")
+        }
+
+        override fun onRegistrationFailed(serviceInfo: NsdServiceInfo, errorCode: Int) {
+            // Registration failed! Put debugging code here to determine why.
+            Log.d(TAG, "Register Failed")
+        }
+
+        override fun onServiceUnregistered(arg0: NsdServiceInfo) {
+            // Service has been unregistered. This only happens when you call
+            // NsdManager.unregisterService() and pass in this listener.
+            Log.d(TAG, "Unregistered")
+        }
+
+        override fun onUnregistrationFailed(serviceInfo: NsdServiceInfo, errorCode: Int) {
+            // Unregistration failed. Put debugging code here to determine why.
+            Log.d(TAG, "Unregistered Failed")
+        }
+    }
+    val discoveryListener = object : NsdManager.DiscoveryListener {
+
+        // Called as soon as service discovery begins.
+        override fun onDiscoveryStarted(regType: String) {
+            Log.d(TAG, "Service discovery started")
+        }
+
+        override fun onServiceFound(service: NsdServiceInfo) {
+            // A service was found! Do something with it.
+            Log.d(TAG, "Service discovery success $service")
+            when {
+                //for some reason service.serviceType adds . to the string
+                service.serviceType != SERVICE_TYPE ->
+                    Log.d(TAG, "Unknown Service Type: ${service.serviceType}")
+                service.serviceName == mServiceName -> // The name of the service tells the user what they'd be
+                    // connecting to. It could be "Bob's Chat App".
+//                    Log.d(TAG, "Same machine: $mServiceName")
+                    nsdManager.resolveService(
+                        service,
+                        resolveListener
+                    )
+                service.serviceName.contains("NsdChat") -> nsdManager.resolveService(
+                    service,
+                    resolveListener
+                )
+            }
+        }
+
+        override fun onServiceLost(service: NsdServiceInfo) {
+            // When the network service is no longer available.
+            // Internal bookkeeping code goes here.
+            Log.e(TAG, "service lost: $service")
+        }
+
+        override fun onDiscoveryStopped(serviceType: String) {
+            Log.i(TAG, "Discovery stopped: $serviceType")
+        }
+
+        override fun onStartDiscoveryFailed(serviceType: String, errorCode: Int) {
+            Log.e(TAG, "Discovery failed: Error code:$errorCode")
+            nsdManager.stopServiceDiscovery(this)
+        }
+
+        override fun onStopDiscoveryFailed(serviceType: String, errorCode: Int) {
+            Log.e(TAG, "Discovery failed: Error code:$errorCode")
+            nsdManager.stopServiceDiscovery(this)
+        }
+    }
+    private val resolveListener = object : NsdManager.ResolveListener {
+        override fun onResolveFailed(serviceInfo: NsdServiceInfo, errorCode: Int) {
+            // Called when the resolve fails. Use the error code to debug.
+            Log.e(TAG, "Resolve failed: $errorCode")
+        }
+
+        override fun onServiceResolved(serviceInfo: NsdServiceInfo) {
+            Log.e(TAG, "Resolve Succeeded. $serviceInfo")
+
+            if (serviceInfo.serviceName == mServiceName) {
+                Log.d(TAG, "Same IP.")
+//                return
+            }
+            val mService = serviceInfo
+            val port: Int = serviceInfo.port
+            val host: InetAddress = serviceInfo.host
+
+            //connect to serversocket based on host/port information acquired from NSD discover
+            val socket = Socket(host,port)
+
+            //outgoing stream redirect to socket
+            val out = DataOutputStream(socket.getOutputStream())
+
+            out.writeUTF("This is the first type of message.")
+            out.writeUTF("This is the second type of message.")
+            out.writeUTF("This is the third type of message (Part 1).")
+            out.writeUTF("This is the third type of message (Part 2).")
+            out.flush() // Send off the data
+            out.close()
+
+            //Close connection
+            socket.close()
+        }
+    }
+
+    override fun onPause() {
+        tearDown()
+        serviceIntent?.let {
+            stopService(it)
+        }
+        super.onPause()
+    }
+
+    fun tearDown() {
+        nsdManager.apply {
+            try {
+                unregisterService(registrationListener)
+            } catch (e: java.lang.IllegalArgumentException) {
+                println(e)
+            }
+            try {
+                stopServiceDiscovery(discoveryListener)
+            } catch (e: java.lang.IllegalArgumentException) {
+                println(e)
+            }
         }
     }
 
@@ -118,6 +321,8 @@ fun MainActivityContent(
                     val deleteButton = createRef()
                     val searchButton = createRef()
                     val settingsButton = createRef()
+                    val hostButton = createRef()
+                    val clientButton = createRef()
                     //loading bar, if it finished doing retrofit then scroll
                     isLoadingState.value?.let { isLoading ->
                         if (isLoading) {
@@ -194,9 +399,48 @@ fun MainActivityContent(
                                 top.linkTo(parent.top)
                                 end.linkTo(parent.end)
                             }
-                            .padding(all = 8.dp), text = "", color = LightBlue, icon = Icons.Filled.Settings
+                            .padding(all = 8.dp),
+                        text = "",
+                        color = LightBlue,
+                        icon = Icons.Filled.Settings
                     ) {
                         navController.navigate("settings")
+                    }
+                    //host button
+                    StatefulObject(
+                        Modifier
+                            .constrainAs(hostButton) {
+                                bottom.linkTo(clientButton.top)
+                                end.linkTo(searchButton.start)
+                            }
+                            .padding(all = 8.dp),
+                        text = "Host",
+                        color = Color.DarkGray,
+                        icon = Icons.Filled.Home
+                    ) {
+                        mainActivity.doBindingService()
+                    }
+                    //client button
+                    StatefulObject(
+                        Modifier
+                            .constrainAs(clientButton) {
+                                bottom.linkTo(parent.bottom)
+                                end.linkTo(searchButton.start)
+                            }
+                            .padding(all = 8.dp),
+                        text = "Client",
+                        color = Color.LightGray,
+                        icon = Icons.Filled.Person
+                    ) {
+                        try {
+                            mainActivity.nsdManager.discoverServices(
+                                mainActivity.SERVICE_TYPE,
+                                NsdManager.PROTOCOL_DNS_SD,
+                                mainActivity.discoveryListener
+                            )
+                        } catch (e: java.lang.IllegalArgumentException) {
+                            println(e)
+                        }
                     }
                 }
             }
@@ -287,8 +531,12 @@ fun MessageCard(msg: Chat) {
                     style = MaterialTheme.typography.body2
                 )
             }
-            if(msg.body.startsWith("http://")||msg.body.startsWith("https://")){
-                GlideImage(model = msg.body,modifier = Modifier.padding(all = 4.dp), contentDescription = "")
+            if (msg.body.startsWith("http://") || msg.body.startsWith("https://")) {
+                GlideImage(
+                    model = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    contentDescription = ""
+                )
             }
         }
     }
---
+++ app/src/main/java/com/example/composer/retrofit/TcpServerService.kt
@@ -0,0 +1,110 @@
+package com.example.composer.retrofit
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Binder
+import android.os.Build
+import android.os.IBinder
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.example.composer.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.io.DataInputStream
+import java.io.DataOutputStream
+import java.io.IOException
+import java.net.ServerSocket
+import java.util.concurrent.atomic.AtomicBoolean
+
+
+class TcpServerService : Service() {
+    private var serverSocket = ServerSocket(0)
+    var port: Int = serverSocket.localPort
+    private val working = AtomicBoolean(true)
+    private val runnable = GlobalScope.launch(Dispatchers.IO) {
+        try {
+            while (working.get()) {
+                println("while loop")
+                val socket = serverSocket.accept()
+                println("New client: $socket")
+                val dataInputStream = DataInputStream(socket.getInputStream())
+                val dataOutputStream = DataOutputStream(socket.getOutputStream())
+
+                while(true){
+                    if (dataInputStream.available() > 0) {
+                        Log.i(TAG, "Received: " + dataInputStream.readUTF())
+                    }
+                }
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+            try {
+                serverSocket.close()
+            } catch (ex: IOException) {
+                ex.printStackTrace()
+            }
+        }
+    }
+
+
+    inner class LocalBinder : Binder() {
+        val service: TcpServerService
+            get() = this@TcpServerService
+    }
+
+    // Create the instance on the service.
+    private val binder = LocalBinder()
+
+    override fun onBind(intent: Intent): IBinder? {
+        return binder
+    }
+
+    override fun onCreate() {
+        startMeForeground()
+        runnable
+    }
+
+    override fun onDestroy() {
+        working.set(false)
+        runnable.cancel()
+        serverSocket.close()
+    }
+
+    //5 second time limit to start a service? it gives ANR when failed to start at that time
+    //https://stackoverflow.com/questions/44425584/context-startforegroundservice-did-not-then-call-service-startforeground
+    fun startMeForeground() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val NOTIFICATION_CHANNEL_ID = packageName
+            val channelName = "Tcp Server Background Service"
+            val chan = NotificationChannel(
+                NOTIFICATION_CHANNEL_ID,
+                channelName,
+                NotificationManager.IMPORTANCE_NONE
+            )
+            chan.lightColor = Color.BLUE
+            chan.lockscreenVisibility = Notification.VISIBILITY_PRIVATE
+            val manager = (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
+            manager.createNotificationChannel(chan)
+            val notificationBuilder = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+            val notification = notificationBuilder.setOngoing(true)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("Tcp Server is running in background")
+                .setPriority(NotificationManager.IMPORTANCE_MIN)
+                .setCategory(Notification.CATEGORY_SERVICE)
+                .build()
+            startForeground(2, notification)
+        } else {
+            startForeground(1, Notification())
+        }
+    }
+
+    companion object {
+        private val TAG = TcpServerService::class.java.simpleName
+    }
+}
